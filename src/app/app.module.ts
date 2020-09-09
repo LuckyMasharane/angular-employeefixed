bd464cb167867c9618352e02a52fd4cd3af0b1d1
LuckyMasharane <lmasharane@gmail.com>
2020-09-09 13:42:00 +0200

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
@@ -13,9 +14,10 @@ import { HelloComponent } from './hello.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { DialogComponent } from './dialog/dialog.component';
 import { DepartmentComponent } from './department/department.component';
+import { AppRoutingMadule } from './app-routing/app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatCardModule, MatButtonModule,  MatGridListModule],
+  imports:      [ BrowserModule, FormsModule, MatCardModule, MatButtonModule,  MatGridListModule, AppRoutingMadule ],
   declarations: [ AppComponent, HelloComponent, EmployeeComponent, DialogComponent, DepartmentComponent ],
   entryComponents:[ DialogComponent ],
   bootstrap:    [ AppComponent ],
