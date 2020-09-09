a4961e91c4cdfcf3b63377956acbbcccd95d5941
LuckyMasharane <lmasharane@gmail.com>
2020-09-09 17:59:11 +0200

    1 file modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeeComponent } from '../employee/employee.component';
 import { DepartmentComponent } from '../department/department.component';
+import { ProjectsComponent } from '../projects/projects.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRouting: Routes = [
