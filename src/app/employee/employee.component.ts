58ec5728984309747ec2c1470e22c2a09d62c446
LuckyMasharane <lmasharane@gmail.com>
2020-09-14 12:46:05 +0200

    1 file modified

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { DialogComponent } from '..s/dialog/dialog.component';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-employee',
