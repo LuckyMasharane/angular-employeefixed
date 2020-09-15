74a2b3e01e1ab7bd05d68956f29ac3786be4224b
LuckyMasharane <lmasharane@gmail.com>
2020-09-15 11:09:05 +0200

    3 files modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,12 +5,14 @@ import { DepartmentComponent } from '../department/department.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { WelcomePageComponent } from '../welcome-page/welcome-page.component';
 import { Routes, RouterModule } from '@angular/router';
+import { AddEmployeeComponent } from '../add-employee/add-employee.component';
 
 const appRouting: Routes = [
   { path: '', component: WelcomePageComponent },
   { path: 'employee', component: EmployeeComponent },
   { path: 'department', component: DepartmentComponent },
-  { path: 'projects', component: ProjectsComponent }
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'add-employee', component: AddEmployeeComponent}
   ];
 
 
